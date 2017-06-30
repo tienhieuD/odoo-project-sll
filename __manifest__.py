507628d7c8a3eff4e893384ee80ca0b2fcf59c71
Tiến Hiệu <duongnguyen1216@gmail.com>
2017-06-30 13:43:57 +0700

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sổ liên lạc điện tử",
+
+    'summary': """Chia Nhân Sự và Phòng ban trong công ty""",
+
+    'description': """
+        Chia Nhân Sự và Phòng ban trong công ty
+    """,
+
+    'author': "BMSGROUP GLOBAL",
+    'website': "http://www.bmsgroupglobal.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'BMS APP',
+    'version': '1.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['crm','product','sale','sale_crm'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'templates.xml',
+        # 'user_groups.xml',
+        #'views/giaoanchinhsua.xml',
+    ],
+}
