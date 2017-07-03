b9b89de4dc6e8d0ad329ccefd3758a0da07d77f9
Tiến Hiệu <duongnguyen1216@gmail.com>
2017-07-03 13:24:38 +0700

    :+1: Views

--- solienlacH.py
+++ solienlacH.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 
 class giaovien(models.Model):
     _name = 'solienlac.giaovien'
+    _rec_name = 'hoten' # optional
     magiaovien = fields.Char("Mã giáo viên")
     hoten = fields.Char("Họ tên")
     gioitinh = fields.Selection([
@@ -26,6 +27,7 @@ class giaovien(models.Model):
 
 class to(models.Model):
     _name = 'solienlac.to'
+    _rec_name = 'tento' # optional
     mato = fields.Char("Mã tổ")
     tento = fields.Char("Tên tổ")
     ghichu = fields.Char("Ghi chú")
@@ -33,6 +35,7 @@ class to(models.Model):
 
 class phuongxa(models.Model):
     _name = 'solienlac.phuongxa'
+    _rec_name = 'tenphuongxa' # optional
     maphuongxa = fields.Char("Mã phường/xã")
     tenphuongxa = fields.Char("Tên phường/xã")
     ghichu = fields.Char("Ghi chú")
@@ -40,25 +43,30 @@ class phuongxa(models.Model):
 
 class quanhuyen(models.Model):
     _name = 'solienlac.quanhuyen'
+    _rec_name = 'tenquanhuyen' # optional
+    _description = 'Module description'
     maquanhuyen = fields.Char("Mã quận/huyện")
     tenquanhuyen = fields.Char("Tên quận/huyện")
     ghichu = fields.Char("Ghi chú")
-    tinhthanhpho = fields.Many2one('solienlac.quanhuyen', string = "Tỉnh/Thành phố")
+    tinhthanhpho = fields.Many2one('solienlac.tinhthanhpho', string = "Tỉnh/Thành phố")
 
 class tinhthanhpho(models.Model):
     _name = 'solienlac.tinhthanhpho'
+    _rec_name = 'tentinhthanhpho' # optional
     matinhthanhpho = fields.Char("Mã tỉnh/thành phố")
     tentinhthanhpho = fields.Char("Tên tỉnh/thành phố")
     ghichu = fields.Char("Ghi chú")
 
 class dantoc(models.Model):
     _name = 'solienlac.dantoc'
+    _rec_name = 'tendantoc' # optional
     madantoc = fields.Char("Mã dân tộc")
     tendantoc = fields.Char("Tên dân tộc")
     ghichu = fields.Char("Ghi chú")
 
 class tongiao(models.Model):
     _name = 'solienlac.tongiao'
+    _rec_name = 'tentongiao' # optional
     matongiao = fields.Char("Mã tôn giáo")
     tentongiao = fields.Char("Tên tôn giáo")
     ghichu = fields.Char("Ghi chú")
@@ -66,6 +74,7 @@ class tongiao(models.Model):
 class phongban(models.Model):
     """docstring for phongban."""
     _name = 'solienlac.phongban'
+    _rec_name = 'tenphongban' # optional
     maphongban = fields.Char("Mã phòng ban")
     tenphongban = fields.Char("Tên phòng ban")
     sdt = fields.Char("Tên phòng ban")
@@ -74,44 +83,68 @@ class phongban(models.Model):
 
 class doituongchinhsach(models.Model):
     _name = 'solienlac.doituongchinhsach'
+    _rec_name = 'tendoituongchinhsach' # optional
     tendoituongchinhsach = fields.Char(string='Tên Đối Tượng Chinh Sách')
     miengiam = fields.Float(string='Miễn Giảm')
     ghichu = fields.Char(string='Ghi Chú')
 
+class doituonguutien(models.Model):
+    _name = 'solienlac.doituonguutien'
+    _rec_name = 'tendoituonguutien' # optional
+    tendoituonguutien = fields.Char(string='Tên Đối Tượng Ưu tiên')
+    madoituonguutien = fields.Char(string='Mã Đối Tượng Ưu tiên')
+    ghichu = fields.Char(string='Ghi Chú')
+
 class khenthuongkyluat(models.Model):
     _name = 'solienlac.khenthuongkyluat'
+    _rec_name = 'tenkhenthuongkyluat' # optional
     tenkhenthuongkyluat = fields.Char(string='Tên Khen Thưởng Kỷ Luật')
     hinhthuckhenthuongkyluat = fields.Char(string='Hình Thức Khen Thưởng Kỷ Luật')
     ghichu = fields.Char(string='Ghi Chú')
 
 class khenthuongkyluat_hocsinh(models.Model):
     _name = 'solienlac.khenthuongkyluat_hocsinh'
+    _rec_name = 'ngay_ktkl' # optional
     ktkl = fields.Many2one('solienlac.khenthuongkyluat', string='ID Khen Thưởng Kỷ Luật', required='True')
     hocsinh = fields.Many2one('solienlac.hocsinh', string='ID Học Sinh', required='True')
     ngay_ktkl = fields.Date(string='Ngày Khen Thưởng Kỷ Luật')
 
 class tuyenhoc(models.Model):
     _name = 'solienlac.tuyenhoc'
+    _rec_name = 'tentuyen' # optional
     matuyen = fields.Char(string='Mã Tuyến')
     tentuyen = fields.Char(string='Tên Tuyến')
     ghichu = fields.Char(string='Ghi Chú')
 
 class khoi(models.Model):
     _name = 'solienlac.khoi'
+    _rec_name = 'tenkhoi' # optional
     makhoi = fields.Char(string='Mã Khối', required='True')
     tenkhoi = fields.Char(string='Tên Khối')
     ghichu = fields.Char('Ghi Chú')
 
 class hanhkiem(models.Model):
     _name = 'solienlac.hanhkiem'
+    _rec_name = 'xeploai' # optional
     hocky = fields.Char('Học Kỳ')
     namhoc = fields.Char('Năm Học')
+    xeploai = fields.Selection(
+        string="Xếp loại",
+        selection=[
+                ('tot', 'Tốt'),
+                ('kha', 'Khá'),
+                ('tb', 'Trung bình'),
+                ('yeu', 'Yếu'),
+                ('kem', 'Kém'),
+        ],
+    )
     nhanxetcuagiaovien = fields.Char('Nhận Xét Của Giáo Viên')
     ghichu = fields.Char('Ghi Chú')
-    #hocsinh = fields.Many2many('solienlac.hocsinh', string='Học Sinh')
+    hocsinh = fields.Many2many('solienlac.hocsinh', string='Học Sinh')
 
 class phuhuynh(models.Model):
     _name = 'solienlac.phuhuynh'
+    _rec_name = 'hoten' # optional
     hoten = fields.Char('Họ Tên')
     gioitinh = fields.Char('Giới Tính')
     ngaysinh = fields.Date('Ngày Sinh')
@@ -123,12 +156,14 @@ class phuhuynh(models.Model):
 
 class bomon(models.Model):
     _name = 'solienlac.bomon'
+    _rec_name = 'tenbomon' # optional
     mabomon = fields.Char('Mã bộ môn')
     tenbomon = fields.Char('Tên bộ môn')
     truongbomon = fields.Many2one('solienlac.giaovien', string = "Trưởng bộ môn")
 
 class bangdiemdanh(models.Model):
     _name = 'solienlac.bangdiemdanh'
+    _rec_name = 'mabangdiemdanh' # optional
     mabangdiemdanh= fields.Char('Mã bảng điểm danh')
     ngayvang= fields.Date('Ngày vắng')
     tietvang= fields.Integer('Tiết vắng')
@@ -139,6 +174,7 @@ class bangdiemdanh(models.Model):
 
 class hocsinh(models.Model):
     _name = 'solienlac.hocsinh'
+    _rec_name = 'hoten' # optional
     mahocsinh = fields.Char('Mã học sinh', required='True')
     hoten = fields.Char('Họ tên', required='True')
     gioitinh = fields.Selection([
@@ -160,6 +196,7 @@ class hocsinh(models.Model):
 
 class lop(models.Model):
     _name = 'solienlac.lop'
+    _rec_name = 'tenlop' # optional
     malop = fields.Char('Mã lớp', required='True')
     tenlop = fields.Char('Tên lớp', required='True')
     nienkhoa = fields.Char('Niên khóa', required='True')
@@ -168,6 +205,7 @@ class lop(models.Model):
 
 class banhoc(models.Model):
     _name = 'solienlac.banhoc'
+    _rec_name = 'tenban' # optional
     maban = fields.Char('Mã ban', required='True')
     tenban = fields.Char('Tên ban')
     monhoc = fields.Char('Môn học')
@@ -175,6 +213,7 @@ class banhoc(models.Model):
 
 class monhoc(models.Model):
     _name = 'solienlac.monhoc'
+    _rec_name = 'tenmonhoc' # optional
     mamonhoc = fields.Char('Mã môn học', required='True')
     tenmonhoc = fields.Char('Tên môn học', required='True')
     heso = fields.Float('Hệ số')
@@ -185,6 +224,7 @@ class monhoc(models.Model):
 
 class ketquahoctap(models.Model):
     _name = 'solienlac.ketquahoctap'
+    _rec_name = 'hocsinh' # optional
     hocsinh = fields.Many2one('solienlac.hocsinh', string='Học sinh')
     monhoc = fields.Many2one('solienlac.monhoc', string='Môn học')
     giaovien = fields.Many2one('solienlac.giaovien', string='Giáo viên')
@@ -196,6 +236,7 @@ class ketquahoctap(models.Model):
 
 class loaidiem(models.Model):
     _name = 'solienlac.loaidiem'
+    _rec_name = 'tenloai' # optional
     maloai = fields.Char('Mã loai', required='True')
     tenloai = fields.Char('Tên loai', required='True')
     heso = fields.Float('Hệ số')
@@ -203,6 +244,7 @@ class loaidiem(models.Model):
 
 class diem(models.Model):
     _name = 'solienlac.diem'
+    _rec_name = 'diem' # optional
     diem = fields.Float('Điểm')
     ngaynhap = fields.Date('Ngày nhập')
     ghichu = fields.Char('Ghi chú')
@@ -212,12 +254,14 @@ class diem(models.Model):
 
 class chucvu(models.Model):
     _name = 'solienlac.chucvu'
+    _rec_name = 'tenchucvu' # optional
     machucvu = fields.Char('Mã chức vụ', required='True')
     tenchucvu = fields.Char('Tên chức vụ', required='True')
     ghichu = fields.Char('Ghi chú')
 
 class nenep(models.Model):
     _name = 'solienlac.nenep'
+    _rec_name = 'hocsinh' # optional
     dongphuc = fields.Integer('Đồng phục')
     dihocmuon = fields.Integer('Đồng phục')
     noichuyen = fields.Integer('Đồng phục')
