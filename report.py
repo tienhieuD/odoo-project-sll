61f94aca4341b142526ce1e4ba828f0ef1e7e990
Tiến Hiệu <duongnguyen1216@gmail.com>
2017-07-13 15:02:56 +0700

    Update baocao 1d

--- report.py
+++ report.py
@@ -4,35 +4,66 @@ from odoo import models, fields, api
 class xlhkhshnkt(models.Model):
     '''Xếp loại hạnh kiểm học sinh hòa nhập, khuyết tật'''
     _name = 'solienlac.bc.xlhkhshnkt'
+    hocky = fields.Selection(
+        string="Học kỳ",
+        selection=[
+                ('i', 'Học kỳ I'),
+                ('ii', 'Học kỳ II'),
+                ('iii', 'Cả năm'),
+        ],
+    )
+    namhoc = fields.Char('Năm học')
     khoi = fields.Many2one(
         string="Khối",
         comodel_name="solienlac.khoi",
     )
-    tongsohs = fields.Integer()
-    tot_sl = fields.Integer()
-    # tot_pt = fields.Float(compute='_compute_tot_pt', store=True)
-    # kha_sl = fields.Integer(compute='_compute_kha_sl', store=True)
-    # kha_pt = fields.Float(compute='_compute_kha_pt', store=True)
-    # tb_sl = fields.Integer(compute='_compute_tb_sl', store=True)
-    # tb_pt = fields.Float(compute='_compute_tb_pt', store=True)
-    # y_sl = fields.Integer(compute='_compute_y_sl', store=True)
-    # y_pt = fields.Float(compute='_compute_y_pt', store=True)
+    tongsohs = fields.Integer('Tổng số học sinh')
+    tot_sl = fields.Integer('Số lượng HK Tốt')
+    tot_pt = fields.Float('Phần trăm HK Tốt')
+    kha_sl = fields.Integer('Số lượng HK Khá')
+    kha_pt = fields.Float('Phần trăm HK Khá')
+    tb_sl = fields.Integer('Số lượng HK Trung bình')
+    tb_pt = fields.Float('Phần trăm HK Trung bình')
+    y_sl = fields.Integer('Số lượng HK Yếu')
+    y_pt = fields.Float('Phần trăm HK Yếu')
+
     @api.multi
     @api.onchange('khoi')
     def _compute_model(self):
-        # Tổng số học sinh khuyết tật
-        self.tongsohs = self.env['solienlac.hocsinh'].search_count(
-            [
+        def laysoluong_hs(hanhkiem):
+            return self.env['solienlac.hocsinh'].search_count(
+                [
+                    ('lop.khoi.id','=',self.khoi.id),
+                    ('khuyettat','!=','value1'),
+                    ('hanhkiem.xeploai','=', hanhkiem),
+                    ('hanhkiem.hocky', '=', self.hocky),
+                    ('hanhkiem.namhoc', '=', self.namhoc),
+                ]
+            )
+        def layphantram_hs(x,y):
+            try:
+                return round(float(x)/float(y)*100, 2)
+            except:
+                return 0.0
+        _tongsohs = self.env['solienlac.hocsinh'].search_count([
                 ('lop.khoi.id','=',self.khoi.id),
-                ('khuyettat','!=','value1')
-            ]
-        )
-        # Số lượng tốt
-        self.tot_sl = self.env['solienlac.hocsinh'].search_count(
-            [
-                ('lop.khoi.id','=',self.khoi.id),
-                '&', ('khuyettat','!=','value1'),
-                ('hanhkiem.xeploai','=','tot'),
-            ]
-        )
+                ('khuyettat','!=','value1'),
+            ])
+        _tot_sl = laysoluong_hs('tot')
+        _kha_sl = laysoluong_hs('kha')
+        _tb_sl  = laysoluong_hs('tb')
+        _y_sl   = laysoluong_hs('yeu')
+        _tot_pt = layphantram_hs(_tot_sl,_tongsohs)
+        _kha_pt = layphantram_hs(_kha_sl,_tongsohs)
+        _tb_pt = layphantram_hs(_tb_sl,_tongsohs)
+        _y_pt = layphantram_hs(_y_sl,_tongsohs)
 
+        self.tongsohs = _tongsohs
+        self.tot_sl = _tot_sl
+        self.tot_pt = _tot_pt
+        self.kha_sl = _kha_sl
+        self.kha_pt = _kha_pt
+        self.tb_sl = _tb_sl
+        self.tb_pt = _tb_pt
+        self.y_sl = _y_sl
+        self.y_pt = _y_pt
